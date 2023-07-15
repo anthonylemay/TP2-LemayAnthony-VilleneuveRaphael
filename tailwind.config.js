df0e1ec74ee332e00a2859f9759ad2ea51efb022
anthonylemay <anthony_lemay@hotmail.com>
2023-07-15 13:26:31 -0400

    ajout accordeon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        flowbite:{
+          100: '#ffffff',
+        }
+      }
+    },
   },
   plugins: [],
 }
