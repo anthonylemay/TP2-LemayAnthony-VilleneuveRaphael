38b9a441a91ac6dcdbb7f6bdfbea71cfa5ecf655
anthonylemay <anthony_lemay@hotmail.com>
2023-07-16 19:04:17 -0400

    commit final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
-  theme: {
-    extend: {
-      colors: {
-        flowbite:{
-          100: '#ffffff',
-        }
-      }
-    },
-  },
   plugins: [],
 }
